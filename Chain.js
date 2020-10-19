b2a97b8795e1ae635b8e1eb01389e7d47999b104
rayipeddi <70923556+rayipeddi@users.noreply.github.com>
2020-10-19 12:54:17 -0700

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,20 @@
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 400
+        }
+        this.chain = Constraint.create(options);
+        this.pointB = pointB
+        World.add(world, this.chain);
+    }
+
+display(){
+    var pointA = this.chain.bodyA.position;
+    var pointB = this.pointB;
+    strokeWeight(4);
+    line(pointA.x, pointA.y, pointB.x, pointB.y);
+}
+}
---
+++ Paper.js
@@ -0,0 +1,26 @@
+class Paper
+{
+    constructor(x,y,radius)
+    {
+        var options = 
+        {
+            'isStatic': false,
+            'restitution': 0.3,
+            'friction': 0.5,
+            'density': 1.2
+        }
+        this.body = Bodies.circle(x, y, radius,options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body)
+    }
+    display(){
+    push();
+    rectMode(CENTER)
+    translate(this.body.position.x, this.body.position.y);
+    strokeWeight(3)
+    fill(255,0,255)
+    ellipse(0,0,this.radius)
+    pop()
+    }
+}
